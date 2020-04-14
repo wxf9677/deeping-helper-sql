eae64f9df086b8bae98866d7677831d552d9b85e
wxf <454036810@qq.com>
2020-04-14 17:13:13 +0800

    asssss

--- src/main/kotlin/com/diving/wsql/temp/SqlTemp2.kt
+++ src/main/kotlin/com/diving/wsql/temp/SqlTemp2.kt
@@ -1,6 +1,5 @@
 package com.diving.wsql.temp
 
-import com.diving.wsql.en.Join
 import com.diving.wsql.temp.en.SQL
 
 data class SqlTemp2( val uk:String, val sql: SQL, val isSuper:Boolean)
--- src/main/kotlin/com/diving/wsql/temp/annotations/SqlQuery.java
+++ src/main/kotlin/com/diving/wsql/temp/annotations/SqlQuery.java
@@ -11,6 +11,7 @@ import java.lang.annotation.Target;
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface SqlQuery {
+    //这个uk只有在做主类查询的时候才会有用
     String uk();
     String tableName();
     boolean distinct() default false;
